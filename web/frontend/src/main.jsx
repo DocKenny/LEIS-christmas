c4f5574cb7de9cfa6cdf88ba479d6663a3ab4ed9
miha.plibersek <miha.plibersek@student.um.si>
2025-12-18 12:14:46 +0100

    Add new styles and components for pixel grid and light rope
    - Introduced new CSS files for global styles, pixel grid, and light rope animations.
    - Removed the old App.css file and integrated styles directly into the components.
    - Updated App.jsx to include new state management for clearing, saving, and loading pixel grid data.
    - Enhanced PixelGrid component to support drawing with mouse events and added functionality for saving/loading pixel states from local storage.
    - Created LightRope component to display animated light elements.
    - Updated main entry point to include LightRope and new styles.

--- web/frontend/src/main.jsx
+++ web/frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-
+import '../public/styles/index.css'
+import LightRope from './lightRope.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LightRope />
+    <div className='main-conteiner'>
+      <App />
+    </div>
   </StrictMode>,
 )

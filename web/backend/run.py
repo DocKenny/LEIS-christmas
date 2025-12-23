ea26fd7ea978a02dc082711e10f100425c9e2e2e
oskar forstnaric <oskar.forstnaric@student.um.si>
2025-12-23 10:10:59 +0100

    Refactor create_app to accept MQTT broker as a parameter and update run.py to pass the broker address

--- web/backend/run.py
+++ web/backend/run.py
@@ -1,5 +1,7 @@
 from app import create_app
 
+MQTT_BROKER = '172.20.10.8'
+
 if __name__ == '__main__':
-    app = create_app()
+    app = create_app(MQTT_BROKER)
     app.run(debug=True, host='0.0.0.0', port=5000)
